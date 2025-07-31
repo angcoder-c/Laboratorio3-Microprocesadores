bfa00625218bf668d8f6363b68daf274f80438d8
angcoder-c <angcoder.c@gmail.com>
2025-07-31 09:41:48 -0600

    problema de entrada multiple resuelto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Laboratorio 3: introduccion a C++
--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
  * Laboratorio 3
  * Angel Gabriel Chavez Otzoy
  * 24248
+ * https://github.com/angcoder-c/Laboratorio3-Microprocesadores
  */
 
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
@@ -75,73 +77,97 @@ int applyOps (string opcode, int op1, int op2, bool invert) {
     int result = 0;
     if (opcode == "000") {
         result = op1 + op2;
-    }
-
-    if (opcode == "001") {
+    } else if (opcode == "001") {
         if (invert) {
             result = op2 - op1;
         } else {
             result = op1 - op2;
         }
 
-    }
-
-    if (opcode == "010") {
+    } else if (opcode == "010") {
         result = op1 * op2;
-    }
-
-    if (opcode == "011") {
+    } else if (opcode == "011") {
             if (invert) {
                 if (op1 == 0) {
                     cout << "ERROR DIVISION POR CERO" << endl;
+                    return NULL;
                 } else {
                     result = op2 / op1;
                 }
             } else {
                 if (op2 == 0) {
                     cout << "ERROR DIVISION POR CERO" << endl;
+                    return NULL;
                 } else {
                     result = op1 / op2;
                 }
             }
-    }
-
-    if (opcode == "101") {
+    } else if (opcode == "101") {
         if (invert) {
             result = pow(op2, op1);
         } else {
             result = pow(op1, op2);
         }
-    }
-
-    if (opcode == "110") {
+    } else if (opcode == "110") {
         if (invert) {
             result = op2 % op1;
         } else {
             result = op1 % op2;
         }
+    } else {
+        cout << "ERROR OPERACION NO SOPORTADA" << endl;
+        return NULL;
     }
     return result;
 }
 
+vector<string> split(string s, char delim) {
+    // separa el input por espacios en blanco
+    vector<string> tokens;
+    string token;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == delim) {
+            tokens.push_back(token);
+            token.clear();
+        } else if (i+1 == s.length()) {
+            token += s[i];
+            tokens.push_back(token);
+            token.clear();
+        }
+        else {
+            token += s[i];
+        }
+    }
+    return tokens;
+}
+
 int main() {
-    string input;
+    string input_str;
     char inmenu = 'x';
 
     while (inmenu != '0') {
         cout << "MENU\n(1) Evaluar operacion\n(0) Salir\n>>>  ";
         cin >> inmenu;
+        cin.ignore();
         if (inmenu == '1') {
             cout << "Input: ";
-            cin >> input;
-            if (validarInput(input)) {
-                int result = applyOps(
-                getOpCode(input),
-                getOp1(input),
-                getOp2(input),
-                opInvert(input)
-                );
-                cout << result << endl;
+            getline(cin, input_str);
+            vector<string> input = split(input_str, ' ');
+            for (int i = 0; i < input.size(); i++) {
+                if (validarInput(input[i])) {
+                    int result = applyOps(
+                    getOpCode(input[i]),
+                    getOp1(input[i]),
+                    getOp2(input[i]),
+                    opInvert(input[i])
+                    );
+                    if (result != NULL) {
+                        cout << result << endl;
+                    }
+                } else {
+                    cout << "ERROR ENTRADA INVALIDA" << endl;
+                }
             }
         }
 
